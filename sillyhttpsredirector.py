cb51cc62148abc0e156e64064c81ea9ff74c9ad2
jordiprats <jordi.prats@gmail.com>
2020-12-06 13:31:36 +0100

    lint

--- sillyhttpsredirector.py
+++ sillyhttpsredirector.py
@@ -12,6 +12,5 @@ def catch_all(u_path):
         content = {'not': 'implemented'}
         return content, status.HTTP_501_NOT_IMPLEMENTED
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000)
